d805afc4172ccac1a62e217d4ce13e36abb4233a
uchaising77 <uchaising10@gmail.com>
2024-12-22 16:58:57 +0600

    Update qsn5.c

--- qsn5.c
+++ qsn5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main()
+{
+float x ,i, c ;
+c=0;
+for(i=0;i<5;i++)
+    {
+        printf("Enter The CGPA: ");
+        scanf("%f",&x);
+        if(x>=3.5 && x<=4.00)
+        {
+            c=c+1;
+        }
+    }
+    printf("%f persons secured above 3.5 CGPA",c);
+return 0;    
+}
+‎
+
 
